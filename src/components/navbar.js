f90f7ea304b78cf1c32343b5947470297f641933
avinash84319 <avichinnu22@gmail.com>
2023-04-14 19:39:47 +0530

    deploy changes 2

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 const Navbar = ()=>{
     return <div className="navbar">
         <div className="navbar-burger">
-            <img  className="menu-image"src="menu.png" alt="dada"></img>
+            {/* <img  className="menu-image"src="menu.png" alt="dada"></img> */}
         </div>
         <div className="navbar-title"><p className="title">Running Avinash.js ...</p></div>
     </div>
