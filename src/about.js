0402b4da750439ac8144d901bb4d86ed4617f8c0
avinash84319 <avichinnu22@gmail.com>
2023-04-16 11:51:54 +0530

    about section complete

--- src/about.js
+++ src/about.js
@@ -16,6 +16,15 @@ const About = function (props) {
                     <button type="button" className="btn"><Link className="link" to='/contact'>Contact Me</Link></button>
                 </div>
                 <div className="text-container">
+                    <p  className="text-name">Avinash Reddy c</p>
+                    <p className="text-head">Web Developer and AI enthusiast</p>
+                    <p className="text">I am B.E student , currently studying in my 4th sem in computer science and bussiness systems
+                    , in JSS Science and Technology University , Mysore . I started my web development in 2nd sem and learnt it all from
+                    different courses . 
+                    Today I know <b>HTML , CSS , JavaScript and basic React </b>and for the backend I use <b>Node and Express </b> . I also know <b>Mongo DB</b> and its cloud sevices to integrate a database in the websites . I have also worked on some basic projects (portfolio) .
+                     I have always been intrested in research and will try to follow it my new found liking Ai and Ml .
+                     Checkout my github and other accounts for more information in contact me page.
+                    </p>
                 </div>
             </div>
         </div>
